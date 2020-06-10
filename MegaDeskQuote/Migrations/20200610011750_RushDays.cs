65cfeeb95a8924c6ce55b99e13384fb8a33110cb
rodriguezhr25 <rodriguezhr25@gmail.com>
2020-06-09 21:42:05 -0400

    Height

---
+++ MegaDeskQuote/Migrations/20200610011750_RushDays.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MegaDeskQuote.Migrations
+{
+    public partial class RushDays : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RushDays",
+                table: "DeskQuote",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RushDays",
+                table: "DeskQuote");
+        }
+    }
+}
