25819024c6f0be23e240c5e6a92548ee8ed456a2
Antonio Lefinir <alefinir@byui.edu>
2020-06-12 21:34:34 -0300

    last changes

--- MegaDeskQuote/wwwroot/js/site.js
+++ MegaDeskQuote/wwwroot/js/site.js
@@ -6,23 +6,7 @@
 
 //alefinir 12062020
 // This is the code to preload the images
-var imageList = Array();
-for (var i = 1; i <= 5; i++) {
-    imageList[i] = new Image(300, 300);
-    imageList[i].src = "http://www.flowsim.se/picts/selec0" + i + "Big.jpg";
-}
 
-function switchImage() {
-    //var selectedImage = document.myForm.switch.options[document.myForm.switch.selectedIndex].value;
-    //document.myImage.src = "../../images" + selectedImage + "Big.jpg";
-    d = document.getElementById("idImg").value;
-    alert(d);
-}
-
-
-function chgImage() {
-    document.getElementById("idImg").src = "../../images" + document.getElementById("idTMaterials").SelectedIndex.value + "Big.jpg";
-}
 
 function showImage() {
     d = $("#idTMaterials option:selected").text();
